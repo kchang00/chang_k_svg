4d13b47b706301352466a3ab1afd013b1cf8a76c
Chang <k_chang@tss-hr420-dm27.fc.ca>
2019-01-21 14:30:17 -0500

    adding inline svg code in html

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # chang_k_svg
 
-01/14 2:46pm
+01/14 2:46pm - Kayla
 
 Folder structure created. First commit.
+
+3:22pm
+
+Updated HTML.
+
+3:50pm dev.kc.html.inClass
+
+Uploaded example SVG and JPEG. Updated index and CSS.
+
+4:17pm dev.kc.script.inClass
+
+Modified index. Updated JS.
+
+01/21 2:13pm - Kayla
---
+++ js/main.js
@@ -0,0 +1,13 @@
+(() => {
+	// self-invoking anonymous function (or Lambda)
+
+	var images = document.querySelectorAll('img') // Asks document to find CSS selector and give it to us
+	
+	// Remember: var => variable. Placeholder to reference things
+	// document is always the webpage
+	// anything after the . is a method or property
+	// . = a method, something you can do (etc. width, height. colour, query selector)
+	
+	console.log(images);
+
+})();
