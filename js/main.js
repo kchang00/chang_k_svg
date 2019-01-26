4e1dcb56b90d02cbe00e2b9d1238a9ba69af52b8
kchang00 <k_chang@fanshaweonline.ca>
2019-01-26 11:36:21 -0500

    modified JS

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 (() => {
 
-	// // this selects the button under the first SVG
- //    var angler_button = document.querySelector("#angler_button");
+// Button Version JS 
 
- //    // this selects the inline SVG - I added an ID to it (see above)
- //    var angler = document.querySelector('#angler');
+/*
+	// this selects the button under the first SVG
+    var angler_button = document.querySelector("#angler_button");
 
- //    // this function runs when you click on the button
- //    function logIcon() {
- //      console.log(this.previousElementSibling.id);
- //    }
+    // this selects the inline SVG - I added an ID to it (see above)
+    var angler = document.querySelector('#angler');
 
- //    // this function runs when you click on the inline SVG
- //    function logMe() {
- //      console.log(this.id);
- //    }
+    // this function runs when you click on the button
+    function logIcon() {
+      console.log(this.previousElementSibling.id);
+    }
 
- //    // event handling for the button
- //    angler_button.addEventListener("click", logIcon);
+    // this function runs when you click on the inline SVG
+    function logMe() {
+      console.log(this.id);
+    }
 
- //    // event handling for the inline SVG
- //    angler.addEventListener("click", logMe)
+    // event handling for the button
+    angler_button.addEventListener("click", logIcon);
+
+    // event handling for the inline SVG
+    angler.addEventListener("click", logMe)
+
+ */
+
+ 	var allIcons = document.querySelectorAll(".icon");
+
+ 	function logIcon() {
+      console.log(this.id);
+  }
+
+  	allIcons.forEach(currenticon => currenticon.
+	addEventListener("click", logIcon));
+
+
 })();
